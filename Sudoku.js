87d87efc0f8202fc4336a274546911c90d219991
chizi <st0511056@gmail.com>
2021-05-27 23:13:20 +0000

    can run

--- Sudoku.js
+++ Sudoku.js
@@ -5,11 +5,8 @@ class Sudoku {
         this.length = row * column;
         this.unknowChar = '.';
         this.chars = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
-        this.qu;
-        this.ans = [];
-    }
-    list(qu) {
-        this.qu = qu;
+        this.done = false;
+        this.multipleSolutions = false;
     }
     set(i, board) {
         const set = board.filter((_, j) => {
@@ -22,8 +19,12 @@ class Sudoku {
     }
     solution(board) {
         let ans = [];
+        if (this.done && !this.multipleSolutions) {
+            return [];
+        }
         let i = board.indexOf(this.unknowChar);
         if (i == -1) {
+            this.done = true
             return [board];
         }
         let set = this.set(i, board);
@@ -33,7 +34,21 @@ class Sudoku {
         }
         return ans;
     }
-    show() {}
+    ans(qu, multipleSolutions = false) {
+        this.multipleSolutions = multipleSolutions;
+        const ans = this.solution(qu).flat(multipleSolutions ? 0 : 1);
+        this.done = false;
+        return ans;
+    }
+    show() {
+        function resize(list, l) {
+            let result = [];
+            for (let i = 0; i < list.length; i++) {
+                result.push(list.splice(0, l));
+            }
+            return result;
+        }
+    }
 }
 
 var list = ["5","3","4",".","7",".",".",".",".",
@@ -46,26 +61,7 @@ var list = ["5","3","4",".","7",".",".",".",".",
             ".",".",".","4","1","9",".",".","5",
             ".",".",".",".","8",".",".","7","9"];
 
-var list2 = ['5', '3', '.', '.', '7', '.', '.', '.', '.',
-             '6', '.', '.', '1', '9', '5', '.', '4', '.',
-             '.', '.', '8', '.', '.', '.', '5', '.', '7',
-             '8', '.', '.', '.', '6', '.', '4', '2', '3',
-             '4', '.', '.', '8', '5', '3', '7', '9', '1',
-             '7', '1', '.', '.', '2', '.', '.', '.', '6',
-             '.', '6', '.', '.', '.', '7', '2', '8', '.',
-             '.', '.', '.', '4', '1', '9', '.', '.', '5',
-             '.', '.', '.', '2', '8', '.', '.', '7', '9'];
 var s = new Sudoku();
-s.list(list2);
-let row = [];
-console.table(resize(s.solution(list2)[0], 9));
-
-function resize(list, l) {
-    let result = [];
-    for (let i = 0; i < list.length; i+=l) {
-        result.push(list.splice(i, l));
-    }
-    return result;
-}
-
-// console.log(s.ans);
+let result = s.ans(list);
+// console.table(resize(result[0], 9));
+console.log(result);
